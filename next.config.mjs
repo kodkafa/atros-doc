e02e4724785a4e95def54a85f3d5fefba43746c9
Cagatay Kaydir <cagataykaydir@gmail.com>
2025-02-06 18:09:26 +0300

    ci: build command

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import nextra from "nextra";
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    unoptimized: true, // mandatory, otherwise won't export
+  },
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: "build"
+};
+
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+export default withNextra(nextConfig);
